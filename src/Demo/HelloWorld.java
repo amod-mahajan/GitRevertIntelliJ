089e61feeb4fc88010fc9eb2a56a879806cf136a
amod.mahajan <amod.mahajan@hotmail.com>
2023-09-18 21:26:44 -0400

    Removed Hello World 3 line

--- src/Demo/HelloWorld.java
+++ src/Demo/HelloWorld.java
@@ -4,6 +4,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello world 1");
-        System.out.println("Hello world 2");
     }
 }
