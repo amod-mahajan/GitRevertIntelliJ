fc6a5371272ba7d930c9c2c6eb53cd4592d320f0
amod.mahajan <amod.mahajan@hotmail.com>
2023-09-18 21:32:24 -0400

    Added B

--- src/Demo/HelloWorld.java
+++ src/Demo/HelloWorld.java
@@ -2,6 +2,6 @@ package Demo;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World A");
+        System.out.println("Hello World B");
     }
 }
